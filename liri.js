30feb07dc5f8d86f151fa8778451395e2a1d30d0
Elvin <elveng@gmail.com>
2019-04-06 19:39:05 -0400

    added omdb query function

--- liri.js
+++ liri.js
@@ -9,26 +9,26 @@ var axios = require("axios");
 
 
 function querySpotify(string) {
-    var searchTrack;
+    var trackName;
     if (string === undefined) {
         // if no song, default to "The Sign" by Ace of Base.
-        //searchTrack = "Ace of Base The Sign";
-        searchTrack = "The Final Countdown";
+        //trackName = "Ace of Base The Sign";
+        trackName = "The Final Countdown";
     } else {
-        searchTrack = parameter;
+        trackName = parameter;
     }
 
     spotify
         .search({
             type: "track",
-            query: searchTrack
+            query: trackName
         })
-        .then( function (response) {
+        .then(function (response) {
             // console.log(response);
-            let artist =  response.tracks.items[0].artists[0].name;
-            let song =    response.tracks.items[0].name;
+            let artist = response.tracks.items[0].artists[0].name;
+            let song = response.tracks.items[0].name;
             let preview = response.tracks.items[0].preview_url;
-            let album =   response.tracks.items[0].album.name;
+            let album = response.tracks.items[0].album.name;
 
             console.log("\n---------------------------------------------------\n");
             console.log("Artist: " + artist);
@@ -36,15 +36,44 @@ function querySpotify(string) {
             console.log("Preview: " + preview);
             console.log("Album: " + album);
             console.log("\n---------------------------------------------------\n");
-        }) 
-        .catch( function (error) {
+        })
+        .catch(function (error) {
             console.log("Error occurred: " + error);
         });
 
 }
 
+function queryOMDB(string) {
+    var movieName = "The Lion King";
 
-querySpotify();
+    var queryUrl = "http://www.omdbapi.com/?t=" + movieName + "&y=&plot=short&apikey=trilogy";
+
+    axios.get(queryUrl).then(
+        function (response) {
+            let title = response.data.Title;
+            let year = response.data.Year;
+            let imdbRating = response.data.imdbRating;
+            let rtRating = response.data.Ratings[1].Value;
+            let country = response.data.Country;
+            let language = response.data.Language;
+            let plot = response.data.Plot;
+            let actors = response.data.Actors;
+
+            //console.log(response.data);
+            console.log (title);
+            console.log (year);
+            console.log (imdbRating);
+            console.log (rtRating);
+            console.log (country);
+            console.log (language);
+            console.log (plot);
+            console.log (actors);
+
+        });
+}
+
+//querySpotify();
+queryOMDB();
 
 var command = process.argv[2];
 var instruction = process.argv[3];
@@ -56,3 +85,6 @@ var instruction = process.argv[3];
 // movie-this
 // do-what-it-says
 
+
+// Improvements
+// - format output text for spacing alignment
