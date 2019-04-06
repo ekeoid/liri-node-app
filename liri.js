7d8eb2a719e924de9354f8f9214927103808d0d9
Elvin <elveng@gmail.com>
2019-04-06 19:20:26 -0400

    added spotify query search

--- liri.js
+++ liri.js
@@ -1,8 +1,50 @@
 require("dotenv").config();
 
-var keys = require(".keys.js");
+var keys = require("./keys.js");
 
-var spotify = new spotify(keys.spotify);
+var Spotify = require("node-spotify-api");
+var spotify = new Spotify(keys.spotify);
+
+var axios = require("axios");
+
+
+function querySpotify(string) {
+    var searchTrack;
+    if (string === undefined) {
+        // if no song, default to "The Sign" by Ace of Base.
+        //searchTrack = "Ace of Base The Sign";
+        searchTrack = "The Final Countdown";
+    } else {
+        searchTrack = parameter;
+    }
+
+    spotify
+        .search({
+            type: "track",
+            query: searchTrack
+        })
+        .then( function (response) {
+            // console.log(response);
+            let artist =  response.tracks.items[0].artists[0].name;
+            let song =    response.tracks.items[0].name;
+            let preview = response.tracks.items[0].preview_url;
+            let album =   response.tracks.items[0].album.name;
+
+            console.log("\n---------------------------------------------------\n");
+            console.log("Artist: " + artist);
+            console.log("Song: " + song);
+            console.log("Preview: " + preview);
+            console.log("Album: " + album);
+            console.log("\n---------------------------------------------------\n");
+        }) 
+        .catch( function (error) {
+            console.log("Error occurred: " + error);
+        });
+
+}
+
+
+querySpotify();
 
 var command = process.argv[2];
 var instruction = process.argv[3];
